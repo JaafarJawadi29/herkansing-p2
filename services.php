b15e2513d893a5d439be8c4bec145b53af10c362
Hajo300 <89904094+Hajo300@users.noreply.github.com>
2022-12-01 14:52:52 +0100

    Removed unused files

---
+++ services.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Services</title>
+    <link rel="stylesheet" href="css/stylesheet.css">
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+    <h1>hoi</h1>
+</body>
+
+</html>
