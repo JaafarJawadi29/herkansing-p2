950823ab418b1c6acab6b8ab49e94c91882d3089
iiGiovanni <Giovanni.de.vries@hotmail.com>
2022-12-07 15:40:19 +0100

    Login system is working now

---
+++ database/authentication.php
@@ -0,0 +1,25 @@
+<?php
+//connect to the server and select database
+$con = mysqli_connect("localhost", "root", "");
+mysqli_select_db($con, "service-it");
+
+// Get values from form in login.php file
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+
+//to prevent from mysqli injection  
+$username = stripcslashes($username);
+$password = stripcslashes($password);
+$username = mysqli_real_escape_string($con, $username);
+$password = mysqli_real_escape_string($con, $password);
+
+//Query the database for user
+$result = mysqli_query($con, "select * from accounts where username = '$username' and password = '$password'")
+    or die("Failed to query database " . mysqli_error($con));
+$row = mysqli_fetch_array($result);
+if ($row['username'] == $username && $row['password'] == $password) {
+    echo "Login success!!!";
+} else {
+    echo "Failed to login!";
+}
--- login.php
+++ login.php
@@ -12,6 +12,7 @@
 <body>
     <?php include 'assets/header.php'; ?>
     <?php include 'assets/database.php'; ?>
+
     <div class="popup">
         <div class="popup-content" id="popup">
             <h1>Wachtwoord vergeten</h1>
@@ -22,11 +23,10 @@
         <div class="container">
             <img class="logo-login" src="assets/svg/Image1.svg" alt="Logo van ServiceIT">
             <h1>Inloggen</h1>
-
-            <form class="login" action="" method="post">
+            <form action="database/authentication.php" class="login" method="post">
                 <div class="input-container">
-                    <input type="text" placeholder="Gebruikersnaam" name="username" required>
-                    <input type="password" placeholder="Wachtwoord" name="password" required>
+                    <input type="text" placeholder="Gebruikersnaam" id="user" name="username" required>
+                    <input type="password" placeholder="Wachtwoord" id="pass" name="password" required>
                     <button class="btn psw" type="button" onclick="openPopup()">Wachtwoord vergeten?</button>
                     <button class="button-1 login" type="submit">Inloggen</button>
             </form>
@@ -42,6 +42,24 @@
             function closePopup() {
                 popup.classList.remove("open-popup");
             }
+
+            // function validation() {
+            //     var id = document.f1.user.value;
+            //     var ps = document.f1.pass.value;
+            //     if (id.length == "" && ps.length == "") {
+            //         alert("User Name and Password fields are empty");
+            //         return false;
+            //     } else {
+            //         if (id.length == "") {
+            //             alert("User Name is empty");
+            //             return false;
+            //         }
+            //         if (ps.length == "") {
+            //             alert("Password field is empty");
+            //             return false;
+            //         }
+            //     }
+            // }
         </script>
 
 </body>
