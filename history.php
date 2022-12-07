b5e01781b2edf9b3e351e87420da017b6442303c
JaafarJawadi29 <91067539+JaafarJawadi29@users.noreply.github.com>
2022-12-07 12:50:55 +0100

    changes

---
+++ history.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/stylesheet.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php include 'header.php'; ?>
+    <h1>Geschiedenis</h1>
+    <main>
+        <div class="service">
+            <p>Service IT-Service</p>
+            <p>Aangevraagd op 01-01-2022</p>
+            <button class="button-2">Contract</button>
+        </div>
+        <div class="service">
+            <p>Service IT-Service</p>
+            <p>Aangevraagd op 01-01-2022</p>
+            <button class="button-2">Contract</button>
+        </div>
+    </main>
+</body>
+</html>
