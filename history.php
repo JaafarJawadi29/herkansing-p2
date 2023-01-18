4d6cf938235ce7c7ff334abbbd2c06e6f33badbb
JaafarJawadi29 <91067539+JaafarJawadi29@users.noreply.github.com>
2023-01-18 13:43:57 +0100

    history page code added for getting services by user

--- history.php
+++ history.php
@@ -1,18 +1,46 @@
 <?php
+    //Get customer id from session login
+    $id = $_SESSION['id']; 
+
+    //Make connection with database
     $conn = mysqli_connect("localhost", "root", "")
     or die("Cannot connect to server");
 
+    //Select correct database else error
     mysqli_select_db($conn, "serviceit")
     or die("Could not find database<br>");
-    $query = "SELECT * FROM service";
-
-    $stmt = mysqli_prepare($conn, $query)
-    or die(mysqli_error($conn));
-
-    mysqli_stmt_execute($stmt)
+    
+    //Get email from customer id
+    $queryCustomerData = "SELECT email FROM customer WHERE id = ?"; //Query to get customer email, ? = customer_id
+    $stmt = mysqli_prepare($conn, $queryCustomerData); //Prepare for execution
+    mysqli_stmt_bind_param($stmt, 'i', $id); //Bind $id to ? as int
+    mysqli_stmt_execute($stmt) //Execute query
     or die("Could not execute query");
 
+    //Bind results from database
+    $email = mysqli_stmt_bind_result($stmt, $customer_email);
+    mysqli_stmt_close($stmt); //Closes stmt
+
+    //Get all services from customer email
+    $queryServiceData = "SELECT * FROM service WHERE customer_email = ?"; //Query to get all services from customer email, ? = customer_email
+    $stmt = mysqli_prepare($conn, $queryServiceData); //Prepare for execution
+    mysqli_stmt_bind_param($stmt, 's', $customer_email); //Bind $customer_email to ? as string
+    mysqli_stmt_execute($stmt) //Execute query
+    or die("Could not execute query");
+
+    //Bind results from database
     mysqli_stmt_bind_result($stmt, $service_id, $customer_email, $employee_email, $type, $description, $hardware_description, $date, $status, $contract);
+    mysqli_stmt_close($stmt); //Closes stmt
+
+    // $stmt = mysqli_prepare($conn, "SELECT * FROM customer WHERE email = ?")
+    // or die("Could not fetch from database");
+    // mysqli_stmt_bind_param($stmt, 'i', $id);
+    
+
+    // mysqli_stmt_execute($stmt)
+    // or die("Could not execute query");
+
+    // mysqli_stmt_bind_result($stmt, $service_id, $customer_email, $employee_email, $type, $description, $hardware_description, $date, $status, $contract);
 ?>
 <!DOCTYPE html>
 <html lang="en">
