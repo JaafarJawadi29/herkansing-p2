53122b1e96174be96e51d6453b2390ac3dcb61e6
Hajo300 <89904094+Hajo300@users.noreply.github.com>
2023-01-20 14:10:46 +0100

    fixed history.php

--- history.php
+++ history.php
@@ -31,15 +31,6 @@ mysqli_stmt_execute($stmt) //Execute query
 //Bind results from database
 mysqli_stmt_bind_result($stmt, $service_id, $user_id, $employee_id, $service_type, $subject, $description, $date, $status, $contract);
 
-// $stmt = mysqli_prepare($conn, "SELECT * FROM customer WHERE email = ?")
-// or die("Could not fetch from database");
-// mysqli_stmt_bind_param($stmt, 'i', $id);
-
-
-// mysqli_stmt_execute($stmt)
-// or die("Could not execute query");
-
-// mysqli_stmt_bind_result($stmt, $service_id, $customer_email, $employee_email, $type, $description, $hardware_description, $date, $status, $contract);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -48,7 +39,7 @@ mysqli_stmt_bind_result($stmt, $service_id, $user_id, $employee_id, $service_typ
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/stylesheet.css">
+    <link rel="stylesheet" href="assets/css/stylesheet2.css">
     <title>Geschiedenis</title>
 </head>
 
@@ -56,11 +47,14 @@ mysqli_stmt_bind_result($stmt, $service_id, $user_id, $employee_id, $service_typ
     <?php include './assets/header2.php'; ?>
     <div class="histories">
         <h1>Geschiedenis</h1>
+
         <?php while (mysqli_stmt_fetch($stmt)) {
             if ($status == "closed") {
         ?>
                 <main>
+
                     <div class="history">
+
                         <p class="type"><?php echo $service_type; ?></p>
                         <p>
                             <?php
