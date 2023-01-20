c46b1a032806d49c6c1082d701511946990b5c80
JaafarJawadi29 <91067539+JaafarJawadi29@users.noreply.github.com>
2023-01-20 16:25:19 +0100

    employee service view made and css changes

---
+++ employee_serviceview.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Service employee</title>
+    <link rel="stylesheet" href="assets/css/stylesheet2.css">
+    <?php include "assets/header2.php" ?>
+</head>
+<body>
+    <div class="content">
+        <div class="titleBox">
+            <h1>Overzicht</h1>
+        </div>
+        <div class="statusBox">
+            <p>Status: </p>
+            <a class="button3" href="" target="_blank">Download</a>
+        </div>
+    </div>
+    <div class="servicesEmployee">
+        <div class="requests">
+            <h3>Type:</h3>
+        </div>
+        <div class="requests">
+            <h3>Onderwerp:</h3>
+        </div>
+        <div class="requests">
+            <h3>Beschrijving:</h3>
+        </div>
+</div>
+</body>
+</html>
