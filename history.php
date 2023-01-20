2c8714e6c58e6ca5d1847ad9fb3acb8d1e79d354
JaafarJawadi29 <91067539+JaafarJawadi29@users.noreply.github.com>
2023-01-20 10:20:06 +0100

    history update

--- history.php
+++ history.php
@@ -1,6 +1,7 @@
 <?php
-    //Get customer id from session login
-    $id = $_SESSION['id']; 
+    session_start();
+    //Get user id from session login
+    $user_id = $_SESSION['user_id']; 
 
     //Make connection with database
     $conn = mysqli_connect("localhost", "root", "")
@@ -10,27 +11,26 @@
     mysqli_select_db($conn, "serviceit")
     or die("Could not find database<br>");
     
-    //Get email from customer id
-    $queryCustomerData = "SELECT email FROM customer WHERE id = ?"; //Query to get customer email, ? = customer_id
-    $stmt = mysqli_prepare($conn, $queryCustomerData); //Prepare for execution
-    mysqli_stmt_bind_param($stmt, 'i', $id); //Bind $id to ? as int
+    //Get user id from user
+    $queryUserData = "SELECT user_id FROM user WHERE user_id = ?"; //Query to get customer email, ? = customer_id
+    $stmt = mysqli_prepare($conn, $queryUserData); //Prepare for execution
+    mysqli_stmt_bind_param($stmt, 'i', $user_id); //Bind $user_id to ? as int
     mysqli_stmt_execute($stmt) //Execute query
     or die("Could not execute query");
 
     //Bind results from database
-    $email = mysqli_stmt_bind_result($stmt, $customer_email);
+    $user_id = mysqli_stmt_bind_result($stmt, $user_id);
     mysqli_stmt_close($stmt); //Closes stmt
 
-    //Get all services from customer email
-    $queryServiceData = "SELECT * FROM service WHERE customer_email = ?"; //Query to get all services from customer email, ? = customer_email
+    //Get all services from user id
+    $queryServiceData = "SELECT * FROM service WHERE user_id = ?"; //Query to get all services from customer email, ? = customer_email
     $stmt = mysqli_prepare($conn, $queryServiceData); //Prepare for execution
-    mysqli_stmt_bind_param($stmt, 's', $customer_email); //Bind $customer_email to ? as string
+    mysqli_stmt_bind_param($stmt, 'i', $user_id); //Bind $user_id to ? as string
     mysqli_stmt_execute($stmt) //Execute query
     or die("Could not execute query");
 
     //Bind results from database
-    mysqli_stmt_bind_result($stmt, $service_id, $customer_email, $employee_email, $type, $description, $hardware_description, $date, $status, $contract);
-    mysqli_stmt_close($stmt); //Closes stmt
+    mysqli_stmt_bind_result($stmt, $service_id, $user_id, $employee_id, $service_type, $subject, $description, $date, $status, $contract);
 
     // $stmt = mysqli_prepare($conn, "SELECT * FROM customer WHERE email = ?")
     // or die("Could not fetch from database");
@@ -56,20 +56,16 @@
     <h1>Geschiedenis</h1>
     <?php while(mysqli_stmt_fetch($stmt)) {
         if ($status == "closed") {
-         ?>
+    ?>
     <main>
         <div class="history">
-            <p class="type"><?php echo $type; ?></p>
+            <p class="type"><?php echo $service_type; ?></p>
             <p>
                 <?php
-                    if ($type == "hardware") {
-                        echo $hardware_description;
-                    } else {
-                        echo $description;
-                    }
+                    echo $description;
                 ?>
             </p>
-            <a href="contract.php">
+            <?= "<a href='contract.php?id=" . $service_id . "'>" ?>
                 <button class="button-2">Contract</button>
             </a>
         </div>
